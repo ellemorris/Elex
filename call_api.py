4ca5ecb283eb9c557458ab78152bfe2541fe29dd
ellemorris <elle.morris@live.com>
2014-05-04 16:17:18 -0400

    Added functions to call_api code

--- call_api.py
+++ call_api.py
@@ -3,53 +3,46 @@ import urllib2
 import sqlite3 as lite
 import sys
 
-url = 'http://export.arxiv.org/api/query?search_query=cat:q-fin.ST&max_results=100'
-data = urllib2.urlopen(url).read()
-d =  feedparser.parse(data)
+#create table, given category
+def APIcall(cat):
+	url = 'http://export.arxiv.org/api/query?search_query=cat:'+ cat + '&max_results=100'
+	data = urllib2.urlopen(url).read()
+	d =  feedparser.parse(data)
+	
+	#create table
+	con=lite.connect('test.db')
+	cur = con.cursor()
+	cur.execute("DROP TABLE IF EXISTS articles")
+	cur.execute("CREATE TABLE articles(id TEXT, category TEXT, title TEXT, summary TEXT, author TEXT)")
 
-url2 = 'http://export.arxiv.org/api/query?search_query=cat:cs.MM&max_results=100'
-data2 = urllib2.urlopen(url2).read()
-d2 =  feedparser.parse(data2)
+	#populate table
+	for i in range(len(d['entries'])):
+		cur.execute('''INSERT INTO articles(id, category, title, summary, author)\
+			VALUES (?,?,?,?,?);''', (d.entries[i].id, cat, d.entries[i].title, d.entries[i].summary, d.entries[i].author))
+	con.commit()
+	
+	#retrieve data from table
+	cur.execute('SELECT id, category, title, summary, author FROM articles WHERE category=(?)', (cat,))
+	rows = cur.fetchall()
+	con.close()
+	return rows
 
-#create table
-con=lite.connect('test.db')
-cur = con.cursor()
-cur.execute("DROP TABLE IF EXISTS qfin")
-cur.execute("CREATE TABLE qfin(id TEXT, title TEXT, summary TEXT, author TEXT)")
-
-#create table
-con=lite.connect('test.db')
-cur = con.cursor()
-cur.execute("DROP TABLE IF EXISTS mm")
-cur.execute("CREATE TABLE mm(id TEXT, title TEXT, summary TEXT, author TEXT)")
-
-#populate table
-for i in range(len(d['entries'])):
-	cur.execute('''INSERT INTO qfin(id, title, summary, author)\
-		VALUES (?,?,?,?);''', (d.entries[i].id, d.entries[i].title, d.entries[i].summary, d.entries[i].author))
-con.commit()
-
-#populate table
-for i in range(len(d2['entries'])):
-	cur.execute('''INSERT INTO mm(id, title, summary, author)\
-		VALUES (?,?,?,?);''', (d2.entries[i].id, d2.entries[i].title, d2.entries[i].summary, d2.entries[i].author))
-con.commit()
+#look at table for specific category
+def retrieveRows (cat):
+	rows = APIcall(cat)
+	for row in rows:
+		print row
 
 #get column names
-cur.execute('PRAGMA table_info(qfin)')
-cols = cur.fetchall()
-for c in cols:
-	print c[0], c[1], c[2]
+def colNames():
+	con=lite.connect('test.db')
+	cur = con.cursor()
+	cur.execute('PRAGMA table_info(articles)')
+	cols = cur.fetchall()
+	for c in cols:
+		print c[0], c[1], c[2]
+	con.close()
 
-#retrieve data from table
-cur.execute("SELECT * FROM qfin")
-rows = cur.fetchall()
-for row in rows:
-	print row
+#retrieveRows('q-fin.ST')
+#retrieveRows('cs.MM')
 
-#retrieve data from table
-cur.execute("SELECT * FROM mm")
-rows2 = cur.fetchall()
-for row in rows2:
-	print row
-con.close()
